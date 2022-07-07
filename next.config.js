ff75eeec1e1c8642421345791caa5ad6262caf9a
wahid-ari <wahiid.arii@Gmail.com>
2022-07-07 22:06:03 +0700

    initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["images.unsplash.com", "i.scdn.co"],
+  },
+  env: {
+    SUPABASE_URL: "https://lycnmgrucuhwifljiwsw.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5Y25tZ3J1Y3Vod2lmbGppd3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcxOTQzNzgsImV4cCI6MTk3Mjc3MDM3OH0.9fjEpk8IIOk5cPQirKXz1bIjYRITLAgEJxpu0T78PfY"
+  },
+}
+
+module.exports = nextConfig
