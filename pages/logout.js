f1e2318e13643354aead731fa8d119399b1712c0
wahid-ari <wahiid.arii@Gmail.com>
2022-07-09 17:39:06 +0700

    fix cookie not deleted when logout

--- pages/logout.js
+++ pages/logout.js
@@ -5,7 +5,8 @@ import nookies from 'nookies';
 export default function Logout() {
 
   useEffect(() => {
-    nookies.destroy(null, 'username')
+    nookies.destroy(null, 'username');
+    document.cookie = 'username=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT';
     Router.replace("/login");
   }, []);
 
