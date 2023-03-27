27e7c6b22e76e541844ec14882576fbc716c9990
wahidari <wahiid.ari@gmail.com>
2023-03-27 20:30:26 +0700

    update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   env: {
     SUPABASE_URL: "https://lycnmgrucuhwifljiwsw.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5Y25tZ3J1Y3Vod2lmbGppd3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcxOTQzNzgsImV4cCI6MTk3Mjc3MDM3OH0.9fjEpk8IIOk5cPQirKXz1bIjYRITLAgEJxpu0T78PfY"
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5Y25tZ3J1Y3Vod2lmbGppd3N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk1NzMzMTAsImV4cCI6MTk5NTE0OTMxMH0.Fq-bY7pg62NIaVu63LZZvy-eVjYaAHvuurLdM3QRYBU"
   },
 }
 
